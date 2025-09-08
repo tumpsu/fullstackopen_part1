4c89b38ffecb97b98ee688d8f557b12e11c05faf
Tuomas Keskitalo <keskitalotuomas@outlook.com>
2025-09-08 18:54:39 +0300

    Changed application to fullfill exercise 2 solution.

--- kurssitiedot/src/App.jsx
+++ kurssitiedot/src/App.jsx
@@ -9,9 +9,9 @@ const Part = ({ name, exercises }) => {
 const Content = ({ parts }) => {
   return (
     <div>
-      {parts.map((part, index) => (
-        <Part key={index} name={part.name} exercises={part.exercises} />
-      ))}
+      <Part name={parts[0].name} exercises={parts[0].exercises} />
+      <Part name={parts[1].name} exercises={parts[1].exercises} />
+      <Part name={parts[2].name} exercises={parts[2].exercises} />
     </div>
   )
 }
