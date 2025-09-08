12204df51e3b26cd1485d86a3e0678b89d23b9a2
Tuomas Keskitalo <keskitalotuomas@outlook.com>
2025-09-08 20:20:00 +0300

    Added 1.4: kurssitiedot step4 cahnges.

--- kurssitiedot/src/App.jsx
+++ kurssitiedot/src/App.jsx
@@ -24,26 +24,26 @@ const Total = ({ parts }) => {
 
 const App = () => {
   const course = 'Half Stack application development'
-  const part1 = {
-    name: 'Fundamentals of React',
-    exercises: 10
-  }
-  const part2 = {
-    name: 'Using props to pass data',
-    exercises: 7
-  }
-  const part3 = {
-    name: 'State of a component',
-    exercises: 14
-  }
+  const parts = [
+    {
+      name: 'Fundamentals of React',
+      exercises: 10
+    },
+    {
+      name: 'Using props to pass data',
+      exercises: 7
+    },
+    {
+      name: 'State of a component',
+      exercises: 14
+    }
+  ]
 
   return (
     <div>
-      <h1>{course}</h1>
-      <p>{part1.name} {part1.exercises}</p>
-      <p>{part2.name} {part2.exercises}</p>
-      <p>{part3.name} {part3.exercises}</p>
-      <p>Number of exercises {part1.exercises + part2.exercises + part3.exercises}</p>
+      <Header course={course} />
+      <Content parts={parts} />
+      <Total parts={parts} />
     </div>
   )
 }
